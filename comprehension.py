9baeb8b77e09f192d60a0892959640a4f6901195
Jones <medonica1105@gmail.com>
2017-06-30 14:13:56 +0200

    finished

--- comprehension.py
+++ comprehension.py
@@ -1,33 +1,33 @@
-import random
+import random  # import random module from python library
 
-guessesTaken = 0
+guessesTaken = 0  # Assign 0 to guessesTaken variable
 
-print('Hello! What is your name?')
-myName = input()
+print('Hello! What is your name?')  # call print function with 'Hello! What is your name?' parameter
+myName = input()  # Assign input function to myName variable
 
-number = random.randint(1, 20)
-print('Well, ' + myName + ', I am thinking of a number between 1 and 20.')
+number = random.randint(1, 20)  # Assign random.randint function with 1, 20 parameter to number variable
+print('Well, ' + myName + ', I am thinking of a number between 1 and 20.') # call print function with the given string and myName variable
 
-while guessesTaken < 6:
-    print('Take a guess.')
-    guess = input()
-    guess = int(guess)
+while guessesTaken < 6:  # iterate the inside block of code until the given condition(guessesTaken < 6) is true
+    print('Take a guess.') # call print function with 'Take a guess.' parameter
+    guess = input()  # Assign input function to guess variable
+    guess = int(guess)  # convert guess variable into integer
 
-    guessesTaken += 1
+    guessesTaken += 1  # add 1 to the value of guessesTaken variable
 
-    if guess < number:
-        print('Your guess is too low.')
+    if guess < number:  # with if statement check the given condition(guess < number) is true
+        print('Your guess is too low.')  # call print function with ' Your guess is too low.' parameter
 
-    if guess > number:
-        print('Your guess is too high.')
+    if guess > number:  # with if statement check the given condition(guess > number) is true
+        print('Your guess is too high.')  # call print function with 'Your guess is too high.' parameter
 
-    if guess == number:
-        break
+    if guess == number:  # with if statement check the given condition(guess == number) is true
+        break  # with break statement terminate the current loop
 
-if guess == number:
-    guessesTaken = str(guessesTaken)
-    print('Good job, ' + myName + '! You guessed my number in ' + guessesTaken + ' guesses!')
+if guess == number:  # with if statement check the given condition(guess == number) is true
+    guessesTaken = str(guessesTaken)  # convert guessesTaken variable into string
+    print('Good job, ' + myName + '! You guessed my number in ' + guessesTaken + ' guesses!')  # # call print function with the given string and number and guessesTaken variable
 
-if guess != number:
-    number = str(number)
-    print('Nope. The number I was thinking of was ' + number)
+if guess != number:  # with if statement check the given condition(guess != number) is true
+    number = str(number)  # convert number variable into string
+    print('Nope. The number I was thinking of was ' + number)  # call print function with the given string and number variable
